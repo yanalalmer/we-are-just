885127fbd08be73d27486ec3fc396048cfad42f8
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-03-29 22:09:01 +0200

    added header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,36 @@ import HamburgerMenu from './HamburgerMenu';
 import styled from 'styled-components';
 import * as variables from '../../styles/variables';
 
-export const HContainer = styled.header``;
-export const HSocialMedia = styled.div``;
+export const HContainer = styled.header`
+  height: ${variables.spacing.headerHeight};
+  width: 100%;
+  position: sticky;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 10;
+  background-color: ${variables.colors.white};
+`;
+export const HSocialMedia = styled.div`
+  font-size: 1.2rem;
+  flex: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    margin-right: 1rem;
+    cursor: pointer;
+    @media ${variables.query.md} {
+      margin-right: 0;
+    }
+  }
+  @media ${variables.query.md} {
+    flex: 1;
+    justify-content: space-evenly;
+  }
+`;
 
 const Header = () => {
   const [isOpen, setIsOpen] = React.useState(false);
