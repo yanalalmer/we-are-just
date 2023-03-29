61e49c2e3935739d2342d33fb2a9897b38b4a0f5
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-03-29 22:12:25 +0200

    added header logo and styling

--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+// image
+import logo from '../../assets/svg/logo.svg';
+// styles
+import styled from 'styled-components';
+import * as variables from '../../styles/variables';
+
+export const HLogoContainer = styled.div`
+  flex: 20%;
+  display: flex;
+  justify-content: center;
+
+  @media ${variables.query.md} {
+    flex: 1;
+  }
+`;
+export const HLogoImg = styled.img`
+  height: 50px;
+`;
 
 const HeaderLogo = () => {
-  return <div>HeaderLogo</div>;
+  return (
+    <HLogoContainer>
+      <HLogoImg src={logo} />
+    </HLogoContainer>
+  );
 };
 
 export default HeaderLogo;
