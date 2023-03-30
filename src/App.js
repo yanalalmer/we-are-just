5bddf38fea858eeb3b06433a88c9abe3f0c8764b
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2023-03-30 08:19:41 +0200

    initialised aos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // STYLES
 import { GlobalStyles } from './styles/globalStyles';
 // COMPONENTS
 import { Calendar, Header, Hero, Quotes } from './components';
 
 function App() {
+  AOS.init();
   return (
     <>
       <GlobalStyles />
